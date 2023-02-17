b742980d890c71a515d8df64bb3bf683631e675d
Gloria-Mwamidi <wakilogloria@gmail.com>
2023-02-16 18:41:16 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Print a string
- * Description: use printf
- * Return: 0
+ * main - Entry point
+ * Description: This function prints a message using the printf() function
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -11,3 +11,4 @@ int main(void)
 	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+
