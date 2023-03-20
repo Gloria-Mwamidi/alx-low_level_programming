dd2f6a271c86c110a39a4aa376718f4dd89d2aa0
Gloria-Mwamidi <wakilogloria@gmail.com>
2023-03-20 10:05:13 -0700

    SUM

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,13 +1,5 @@
 #ifndef SUM_H
 #define SUM_H
-
-/**
- * 4-sum.h - header file
- * description: a function-like macro SUM(x, y) 
- * that computes the sum of the numbers x and y
- * 
- */
-
 #define SUM(x, y) ((x) + (y))
 
 #endif
