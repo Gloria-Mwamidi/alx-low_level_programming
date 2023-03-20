267044fbf96e2bb26c3e21925c67f2c270cfe202
Gloria-Mwamidi <wakilogloria@gmail.com>
2023-03-20 07:14:18 -0700

    Function-like macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+/**
+ * 3-function_like_macro.h - computes the absolute value of x
+ */
+
+#define ABS(X) ((X) < 0 ? -(X) : (X))
+
+#endif
