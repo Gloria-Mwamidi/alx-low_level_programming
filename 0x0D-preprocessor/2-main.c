5e1c0e98bff42d6076889c2da14a563dfcce59a6
Gloria-Mwamidi <wakilogloria@gmail.com>
2023-03-20 06:52:03 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - function that prints the file name it was complied from
+ * __FILE__: preprocessor directive
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+		return (0);
+}
