27cf484ff802815caba586932bfa2ec080eb1e69
Gloria-Mwamidi <wakilogloria@gmail.com>
2023-03-23 10:56:32 -0700

    Task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,14 +16,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(prints, n);
 
 	for (i = 0; i < n; i++)
-
-	printf("%d", va_arg(prints, int));
-
 	{
+		printf("%d", va_arg(prints, int));
 		if (i != n - 1 && separator != NULL)
-
+		{
 			printf("%s", separator);
-
+		}
 	}
 
 	va_end(prints);
