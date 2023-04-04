e4e58215434bbb9b5a078a1d8504897ee37fc402
Gloria-Mwamidi <wakilogloria@gmail.com>
2023-04-04 14:18:40 -0700

    Task 13

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,82 @@
+#include "lists.h"
+
+/**
+ * loop_listint_count - a function that counts the number of nodes
+ * @head: a pointer to the node
+ *
+ * Return: the number of nodes on success and 0 on failure
+ */
+size_t loop_listint_count(listint_t *head)
+{
+	listint_t *first, *last;
+	size_t len = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	first = head->next;
+	last = (head->next)->next;
+
+	while (last)
+	{
+		if (first == last)
+		{
+			first = head;
+			while (first != hare)
+			{
+				len++;
+				first = first->next;
+				last = last->next;
+			}
+
+			first = first->next;
+			while (first != last)
+			{
+				len++;
+				first = first->next;
+			}
+
+			return (len);
+		}
+
+		first = first->next;
+		last = (last->next)->next;
+	}
+
+	return (0);
+}
+
+/**
+ * free_listint_safe - a function the frees listint_t
+ * @h: a pointer to the node
+ *
+ * Return: The size of the list that was freed.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *temp;
+	size_t len, index;
+
+	len = loop_listint_count(*h);
+	if (len == 0)
+	{
+		for (; h != NULL && *h != NULL; len++)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+		}
+	}
+	else
+	{
+		for (index = 0; index < len; index++)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+		}
+		*h = NULL;
+	}
+	h = NULL;
+	return (len);
+}
