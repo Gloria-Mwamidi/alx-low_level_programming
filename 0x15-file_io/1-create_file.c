41109dd62f9f962f32d791a8fecb3c6572acead2
Gloria-Mwamidi <wakilogloria@gmail.com>
2023-04-14 14:10:26 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,9 +2,9 @@
 /**
  * create_file - a function that creates a file
  * @filename: a pointer to the file
- * @text_context: is a NULL terminated string to write to the file
+ * @text_content: is a NULL terminated string to write to the file
  *
- * Return: 1 on success, -1 on failure 
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
