afa17ee8d3f3a34ff85e79ec0a639685fb88ee6b
Gloria-Mwamidi <wakilogloria@gmail.com>
2023-04-14 13:32:12 -0700

    Task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * create_file - a function that creates a file
+ * @filename: a pointer to the file
+ * @text_context: is a NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure 
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int x;
+	FILE *fp;
+
+	if (filename == NULL)
+		return (-1);
+	fp = fopen(filename, "w");
+
+	if (fp == NULL)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		x = fputs(text_content, fp);
+
+		if (x < 0)
+		{
+			fclose(fp);
+			return (-1);
+		}
+	}
+	fclose(fp);
+	return (1);
+}
