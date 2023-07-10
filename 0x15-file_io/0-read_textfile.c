78804f5d4d4c1a70d8fa2434162c836dda0cdcb5
Gloria-Mwamidi <wakilogloria@gmail.com>
2023-07-10 04:15:20 -0700

    Mandatory files update and Task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * read_textfile - a function that reads a textfile and
+ * prints it to the POSIX stand output
+ * @filename: the file to be read
+ * @letters: the number of letters it should print and read
+ * Return: on success the actual numbr of letters it could read
+ * and print on failur -1
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t read_bytes;
+	ssize_t write_bytes;
+	char *buff;
+	int file;
+
+	if (filename == NULL)
+		return (0);
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+	{
+		free(buff);
+		return (0);
+	}
+	read_bytes = read(filename, buff, letters);
+	if (read_bytes == -1)
+	{
+		close(read_bytes);
+		free(buff);
+		return (0);
+	}
+	write_bytes = write(STDOUT_FILENO, buff, read_bytes);
+	if (write_bytes == -1 || write_bytes != read_bytes)
+	{
+		close(write_bytes);
+		free(buff);
+		return (0);
+	}
+	else
+	free(buff);
+	close(file);
+	return (read_bytes);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+0x15. C - File I/O - Alx Low Level Programming/ C Programming Language
