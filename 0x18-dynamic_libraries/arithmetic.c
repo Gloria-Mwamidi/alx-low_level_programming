bf700c11405c9d630e818803a3d7b7cf7315b1f3
Gloria-Mwamidi <wakilogloria@gmail.com>
2023-08-21 13:32:55 -0700

    First attempt task 2

---
+++ 0x18-dynamic_libraries/arithmetic.c
@@ -0,0 +1,60 @@
+#include "main.h"
+/**
+ * sum_add - a function that computes the
+ * summation of two integers
+ * @a: first integer
+ * @b: second integer
+ * return: sum
+ */
+int sum_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * modulus - a function that gets the remainder
+ * between two divided integers
+ * @a: first integer
+ * @b: second integer
+ * return: the remainder
+ */
+int modulus(int a, int b)
+{
+	return (a % b);
+}
+
+/**
+ * subtract - a function that subtracts
+ * two integers
+ * @a: first integer
+ * @b: second integer
+ * return: the subtraction
+ */
+int subtract(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * divide - a function that divides
+ * two integrs 
+ * @a: first integer
+ * @b: second integer
+ * return: the division
+ */
+int divide(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * multiple - a function that multiples
+ * two integrs
+ * @a: first integer
+ * @b: second integer
+ * return: the multiple
+ */
+int multiple(int a, int b)
+{
+	return (a * b);
+}
